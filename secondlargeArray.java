08c447cd7b40ac0029bbf2c0a673980f6807a170
Ankita Dhokane <149875064+ankitadhokane24@users.noreply.github.com>
2024-07-22 20:22:22 +0530

    Add files via upload

---
+++ secondlargeArray.java
@@ -0,0 +1,39 @@
+//Java Program to Find 2nd Largest Number in an array
+import java.util.Scanner;
+ class secondlargeArray 
+{
+    	public static void main(String[] args) 
+	{
+        		Scanner s = new Scanner(System.in);
+                   System.out.print("Enter size of array: ");
+        		int n = s.nextInt();
+        		int[] a= new int[n];
+        
+		System.out.println("Enter elements of the array:");
+        		for (int i = 0; i < n; i++) 
+		{
+
+            		a[i] = s.nextInt();
+       		}
+		
+		System.out.println("Array:");
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]+"\t");
+		}
+
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		System.out.println("2nd largest element in array:"+a[n-2]);
+	}
+}
---
+++ secondsmallestArray.java
@@ -0,0 +1,39 @@
+//Java to Program Find 2nd Smallest Number in an array
+import java.util.Scanner;
+ class secondsmallestArray 
+{
+    	public static void main(String[] args) 
+	{
+        		Scanner s = new Scanner(System.in);
+                   System.out.print("Enter size of array: ");
+        		int n = s.nextInt();
+        		int[] a= new int[n];
+        
+		System.out.println("Enter elements of the array:");
+        		for (int i = 0; i < n; i++) 
+		{
+
+            		a[i] = s.nextInt();
+       		}
+		
+		System.out.println("Array:");
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]+"\t");
+		}
+
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		System.out.println("2nd smallest element in array:"+a[1]);
+	}
+}
